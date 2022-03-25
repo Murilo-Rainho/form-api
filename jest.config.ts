debfe290c2ee48e706e8fa9abfa14397aad89477
Murilo Rainho <murilorainho01@gmail.com>
2022-03-25 19:56:04 -0300

    chore: fix path of tests files to jest

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ export default {
   // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/tests/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
@@ -118,7 +118,7 @@ export default {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    '<rootDir>/src',
+    '<rootDir>/tests',
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
