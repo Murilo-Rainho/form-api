4872e51773e05191146aec2ec23ada30cf93b5e4
Murilo Rainho <murilorainho01@gmail.com>
2022-03-26 18:23:04 -0300

    feat: ensure SignUpController returns badRequest (helper) if no password confirmation is provided

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -5,7 +5,7 @@ import { badRequest } from '../helpers';
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['email', 'name', 'password'];
+    const requiredFields = ['email', 'name', 'password', 'passwordConfirmation'];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
