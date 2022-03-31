9165bb0d88610e45229158fd7d982556deb8ab0d
Murilo Rainho <murilorainho01@gmail.com>
2022-03-31 13:05:04 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/emailValidatorAdapter.ts
+++ src/utils/emailValidatorAdapter.ts
@@ -1,7 +1,9 @@
+import validator from 'validator';
+
 import { EmailValidator } from '../presentation/controllers/signUp/signUpProtocols';
 
 export class EmailValidatorAdapter implements EmailValidator {
-  isValid(_email: string): boolean {
-    return false;
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
   }
 }
