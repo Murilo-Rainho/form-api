45a129456a8cb2e390ec5da63e9fb40904c8be1d
Murilo Rainho <murilorainho01@gmail.com>
2022-04-08 09:49:41 -0300

    test: ensure createUserRepository returns an account on success

---
+++ tests/units/infra/database/mongodb/userRepository/userRepository.spec.ts
@@ -0,0 +1,45 @@
+import { config } from 'dotenv';
+
+import { mongoHelper } from '../../../../../../src/infra/database/mongodb/helper';
+
+import { UserRepository } from '../../../../../../src/infra/database/mongodb/userRepository';
+
+config();
+
+interface FactoriesTypes {
+  userRepository: UserRepository;
+}
+
+const factories = (): FactoriesTypes => {
+  const userRepository = new UserRepository();
+
+  return {
+    userRepository,
+  };
+};
+
+describe('mongodb\'s userRepository', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect();
+  });
+
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
+
+  test('Should return an user on success', async () => {
+    const { userRepository } = factories();
+
+    const createdUser = await userRepository.createOne({
+      name: 'My Any Name',
+      email: 'any_email@email.com',
+      password: 'any_password',
+    });
+
+    expect(createdUser).toBeTruthy();
+    expect(createdUser.id).toBeTruthy();
+    expect(createdUser.name).toBe('My Any Name');
+    expect(createdUser.email).toBe('any_email@email.com');
+    expect(createdUser.password).toBe('any_password');
+  });
+});
