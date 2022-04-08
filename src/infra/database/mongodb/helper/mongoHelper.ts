69a9c07d2c0192b48cb0f77f16e31ae252c5df1a
Murilo Rainho <murilorainho01@gmail.com>
2022-04-08 09:09:10 -0300

    chore: change mongoHelper diretory from ./test to ./src

---
+++ src/infra/database/mongodb/helper/mongoHelper.ts
@@ -0,0 +1,14 @@
+import { Collection, MongoClient } from 'mongodb';
+
+export const mongoHelper = {
+  client: null as MongoClient,
+  async connect(): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL);
+  },
+  async disconnect(): Promise<void> {
+    await this.client.close();
+  },
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
+  },
+};
