64312ba243a85675adf6ed379199278edac9fd48
Murilo Rainho <murilorainho01@gmail.com>
2022-04-09 08:20:26 -0300

    refactor: remove '_id' field from response

--- src/infra/database/mongodb/userRepository/userRepository.ts
+++ src/infra/database/mongodb/userRepository/userRepository.ts
@@ -7,16 +7,19 @@ import { mongoHelper } from '../helper';
 
 export class UserRepository implements CreateUserRepository {
   async createOne(userData: UserRequestData): Promise<UserResponseData> {
+    const { name, email, password } = userData;
+
     const userCollection = mongoHelper.getCollection('users');
 
-    const { insertedId: { id } } = await userCollection
-      .insertOne(userData);
+    const { insertedId: { id } } = await userCollection.insertOne(userData);
 
     const userId = id.toString();
 
     const createdUser = {
       id: userId,
-      ...userData,
+      name,
+      email,
+      password,
     };
 
     return createdUser;
