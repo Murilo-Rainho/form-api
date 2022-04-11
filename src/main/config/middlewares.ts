e86e235f4b082ee49f16e75a97d5b1d0af09e137
Murilo Rainho <murilorainho01@gmail.com>
2022-04-10 22:08:16 -0300

    feat: ensure CORS is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,10 @@
 import { Express } from 'express';
-import { bodyParser } from './middlewares/index'; // why index is not found by defaul??
+import {
+  bodyParser,
+  cors,
+} from './middlewares/index'; // why index is not found by defaul??
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
