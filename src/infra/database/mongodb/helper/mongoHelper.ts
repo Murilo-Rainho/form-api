424cdaf90a8682d237c3ee71fda940561dd10110
Murilo Rainho <murilorainho01@gmail.com>
2022-04-15 12:16:37 -0300

    refactor: change mongoHelper.getCollection to async function and it'll check if is connected to db

--- src/infra/database/mongodb/helper/mongoHelper.ts
+++ src/infra/database/mongodb/helper/mongoHelper.ts
@@ -1,14 +1,31 @@
 import { Collection, MongoClient } from 'mongodb';
 
 export const mongoHelper = {
+  isConnected: false,
   client: null as MongoClient,
+  mongoUrl: null as string,
   async connect(mongoUrl: string): Promise<void> {
-    this.client = await MongoClient.connect(mongoUrl);
+    this.mongoUrl = mongoUrl;
+    try {
+      this.client = await MongoClient.connect(mongoUrl);
+      this.isConnected = true;
+    } catch (error) {
+      console.log(error);
+    }
   },
   async disconnect(): Promise<void> {
     await this.client.close();
+    this.isConnected = false;
+    this.client = null;
   },
-  getCollection(name: string): Collection {
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.isConnected) {
+      try {
+        await this.connect(this.mongoUrl);
+      } catch (error) {
+        console.log(error);
+      }
+    }
     return this.client.db().collection(name);
   },
 };
--- src/infra/database/mongodb/userRepository/userRepository.ts
+++ src/infra/database/mongodb/userRepository/userRepository.ts
@@ -9,7 +9,7 @@ export class UserRepository implements CreateUserRepository {
   async createOne(userData: UserRequestData): Promise<UserResponseData> {
     const { name, email, password } = userData;
 
-    const userCollection = mongoHelper.getCollection('users');
+    const userCollection = await mongoHelper.getCollection('users');
 
     const { insertedId: { id } } = await userCollection.insertOne(userData);
 
