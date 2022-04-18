3aabc9c6c3ba6c916193bafddf38e5a7417bde5b
Murilo Rainho <murilorainho01@gmail.com>
2022-04-18 07:07:48 -0300

    refactor: change mongoHelper verification if is connected

--- src/infra/database/mongodb/helper/mongoHelper.ts
+++ src/infra/database/mongodb/helper/mongoHelper.ts
@@ -1,21 +1,18 @@
 import { Collection, MongoClient } from 'mongodb';
 
 export const mongoHelper = {
-  isConnected: false,
   client: null as MongoClient,
   mongoUrl: null as string,
   async connect(mongoUrl: string): Promise<void> {
     this.mongoUrl = mongoUrl;
     this.client = await MongoClient.connect(mongoUrl);
-    this.isConnected = true;
   },
   async disconnect(): Promise<void> {
     await this.client.close();
-    this.isConnected = false;
     this.client = null;
   },
   async getCollection(name: string): Promise<Collection> {
-    if (!this.isConnected) {
+    if (!this.client?.isConnected()) {
       await this.connect(this.mongoUrl);
     }
     return this.client.db().collection(name);
