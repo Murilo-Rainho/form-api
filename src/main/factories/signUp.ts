bfcd0bc589937b2b7e415f335944a6c1ca5f4fbc
Murilo Rainho <murilorainho01@gmail.com>
2022-04-19 08:48:12 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/signUp.ts
+++ src/main/factories/signUp.ts
@@ -1,12 +1,14 @@
+import { Controller } from '../../presentation/protocols';
 import {
   SignUpController,
   EmailValidatorAdapter,
   DbCreateUser,
   BcryptAdapter,
   UserRepository,
+  LogControllerDecorator,
 } from './signUpProtocols';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
   const userRepository = new UserRepository();
@@ -14,6 +16,7 @@ export const makeSignUpController = (): SignUpController => {
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const dbCreateUser = new DbCreateUser(bcryptAdapter, userRepository);
   const signUpController = new SignUpController(emailValidatorAdapter, dbCreateUser);
+  const logControllerDecorator = new LogControllerDecorator(signUpController);
 
-  return signUpController;
+  return logControllerDecorator;
 };
--- src/main/factories/signUpProtocols.ts
+++ src/main/factories/signUpProtocols.ts
@@ -3,3 +3,4 @@ export { EmailValidatorAdapter } from '../../utils';
 export { DbCreateUser } from '../../data/usecases/dbCreateUser';
 export { BcryptAdapter } from '../../infra/cryptography';
 export { UserRepository } from '../../infra/database/mongodb/userRepository';
+export { LogControllerDecorator } from '../decorators';
