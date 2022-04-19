2003f468d7eef9eb26849846458d1af24a0a0f6b
Murilo Rainho <murilorainho01@gmail.com>
2022-04-19 09:08:37 -0300

    test: ensure LogControllerDecorator calls console.log if httpResponse statusCode was '500'

--- tests/units/main/decorators/log.spec.ts
+++ tests/units/main/decorators/log.spec.ts
@@ -1,4 +1,7 @@
 import { LogControllerDecorator } from "../../../../src/main/decorators";
+
+import { ServerError } from "../../../../src/presentation/errors";
+import { internalError } from "../../../../src/presentation/helpers";
 import { Controller, HttpRequest, HttpResponse } from "../../../../src/presentation/protocols";
 
 class ControllerStub implements Controller {
@@ -32,7 +35,7 @@ describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
     const { logControllerDecorator, controllerStub } = factories();
 
-    const spyControllerStub = jest.spyOn(controllerStub, 'handle')
+    const spyControllerStub = jest.spyOn(controllerStub, 'handle');
 
     const httpRequest = {
       body: {
@@ -44,7 +47,7 @@ describe('LogController Decorator', () => {
     };
 
     await logControllerDecorator.handle(httpRequest);
-    expect(spyControllerStub).toHaveBeenCalledWith(httpRequest)
+    expect(spyControllerStub).toHaveBeenCalledWith(httpRequest);
   });
 
   test('Should return controller handle response', async () => {
@@ -60,6 +63,25 @@ describe('LogController Decorator', () => {
     };
 
     const httpResponse = await logControllerDecorator.handle(httpRequest);
-    expect(httpResponse).toEqual({ statusCode: 200, body: { ...httpRequest.body } })
+    expect(httpResponse).toEqual({ statusCode: 200, body: { ...httpRequest.body } });
+  });
+
+  test('Should log error if controller handle statusCode response was \'500\'', async () => {
+    const { logControllerDecorator, controllerStub } = factories();
+
+    jest.spyOn(controllerStub, 'handle').mockImplementationOnce(() => new Promise((resolve) => resolve(internalError())));
+    const spyConsoleLog = jest.spyOn(console, 'log');
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        name: 'My Any Name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    await logControllerDecorator.handle(httpRequest);
+    expect(spyConsoleLog).toHaveBeenCalledWith(new ServerError());
   });
 });
